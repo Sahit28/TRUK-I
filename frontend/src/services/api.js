7d73beeef9bf59441610450d54a842c3cb23f41c
sahit28 <sahitalo@gmail.com>
2025-06-13 11:55:31 -0500

    Guardo cambios locales antes del pull

---
+++ frontend/src/services/api.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL, // usa la variable del .env
+  withCredentials: true
+});
+
+export default instance;
